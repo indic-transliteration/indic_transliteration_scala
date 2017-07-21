4b2d97f86e4f022b140111df2fcb8a301bdd6439
vishvAsaH <vishvas.vasuki@gmail.com>
2017-07-20 18:44:48 -0700

    Fix bugs

--- src/main/scala/sanskritnlp/transliteration/iast.scala
+++ src/main/scala/sanskritnlp/transliteration/iast.scala
@@ -66,13 +66,13 @@ object iastDcs extends IastBase {
   override val romanToDevaContextFreeReplacements = Map(
     "ṃ" -> "ं",  "ḥ" -> "ः", "//" -> "॥", "/" -> "।",
     "'" -> "ऽ", "." -> "॰",
-    "0'" -> "०", "1"-> "१", "2"-> "२",
+    "0" -> "०", "1"-> "१", "2"-> "२",
     "3"-> "३", "4"-> "४", "5"-> "५",
     "6"-> "६", "7"-> "७", "8"-> "८", "9"-> "९", "aum" -> "ॐ")
   override val devaToRomanGeneral = romanToDevaContextFreeReplacements.map(_.swap)
 
   def test_toDevanagari(): Unit = {
-    val romanText = "Asaya auṣadhiḥ granthaḥ! l2kAro'sti. nāsti les4o'pi saṃśayaḥ. Kaaṣṭhaḥ bhoḥ/ 12345" +
+    val romanText = "Asaya auṣadhiḥ granthaḥ! l2kAro'sti. nāsti les4o'pi saṃśayaḥ. Kaaṣṭhaḥ bhoḥ/ 12345 10.2" +
       "Aṃkuśeśvaram// iḍā"
     println("IAST Tests.")
     test_toDevanagari(romanText)
