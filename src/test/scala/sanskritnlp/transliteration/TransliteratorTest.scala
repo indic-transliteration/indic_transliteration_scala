f384221ea0f8d5c046b65a8a17ab77fd4e606373
vishvAsaH <vishvas.vasuki@gmail.com>
2017-11-08 18:38:13 -0800

    Fix test

--- src/test/scala/sanskritnlp/transliteration/TransliteratorTest.scala
+++ src/test/scala/sanskritnlp/transliteration/TransliteratorTest.scala
@@ -6,16 +6,38 @@ import org.scalatest.FlatSpec
 import scala.io.Source
 import org.json4s._
 import org.json4s.JsonDSL._
-import org.json4s.native.JsonMethods._
+import org.json4s.native.{JsonMethods, Serialization}
 import org.slf4j.LoggerFactory
+
+case class ApproxDeduplicationTests(
+                           canonical_source: Option[String],
+                           duplicates: Seq[Seq[String]],
+                           non_duplicates: Seq[Seq[String]]
+                         )
 
 class GetApproxDeduplicatingKeySpec extends FlatSpec {
   val log = LoggerFactory.getLogger(this.getClass)
 
   "getApproxDeduplicatingKey" should "provide the same key for certain cases" in {
     val source = Source.fromResource("approxDeduplicationTests.json")
-    log.debug(source.mkString)
-    assert(transliterator.getApproxDeduplicatingKey(text = "धर्म्म") == "धर्म")
+    implicit val formats = DefaultFormats
+    val approxDeduplicationTests = Serialization.read[ApproxDeduplicationTests](source.mkString)
+    log.debug(approxDeduplicationTests.toString)
+
+    log.info(s"Checking ${approxDeduplicationTests.duplicates.size} duplicate sets.")
+    approxDeduplicationTests.duplicates.foreach(duplicatesList => {
+      val keys = duplicatesList.map(x => transliterator.getApproxDeduplicatingKey(text = x)).toSet
+      log.debug(s"Checking ${duplicatesList.mkString(",")}.")
+      assert(keys.size == 1)
+    })
+
+    log.info(s"Checking ${approxDeduplicationTests.non_duplicates.size} non-duplicate sets.")
+    approxDeduplicationTests.non_duplicates.foreach(nonDuplicatesList => {
+      val keys = nonDuplicatesList.map(x => transliterator.getApproxDeduplicatingKey(text = x)).toSet
+      log.debug(s"Checking ${nonDuplicatesList.mkString(",")}.")
+      assert(keys.size != 1)
+    })
+
   }
 
 }
