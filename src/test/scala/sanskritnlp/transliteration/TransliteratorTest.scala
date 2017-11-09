24fd661ebc705717d10f12f06bedf57c00a5dd9c
vishvAsaH <vishvas.vasuki@gmail.com>
2017-11-08 17:01:58 -0800

    Setup scalatest

---
+++ src/test/scala/sanskritnlp/transliteration/TransliteratorTest.scala
@@ -0,0 +1,21 @@
+package sanskritnlp.transliteration
+
+
+import org.scalatest.FlatSpec
+
+import scala.io.Source
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.native.JsonMethods._
+import org.slf4j.LoggerFactory
+
+class GetApproxDeduplicatingKeySpec extends FlatSpec {
+  val log = LoggerFactory.getLogger(this.getClass)
+
+  "getApproxDeduplicatingKey" should "provide the same key for certain cases" in {
+    val source = Source.fromResource("/approxDeduplicationTests.json")
+    log.debug(source.mkString)
+    assert(transliterator.getApproxDeduplicatingKey(text = "धर्म्म") == "धर्म")
+  }
+
+}
