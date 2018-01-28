1eeb3b035c04e956557af765de304834a1217a42
vishvAsaH <vishvas.vasuki@gmail.com>
2018-01-28 09:18:27 -0800

    Better tests

--- src/test/scala/sanskritnlp/transliteration/TransliterationTest.scala
+++ src/test/scala/sanskritnlp/transliteration/TransliterationTest.scala
@@ -4,27 +4,27 @@ package sanskritnlp.transliteration
 import org.json4s._
 import org.json4s.native.Serialization
 import org.scalatest.FlatSpec
-import org.slf4j.LoggerFactory
+import org.slf4j.{Logger, LoggerFactory}
 
 import scala.io.Source
 
 case class TransliterationTests(
                                  canonical_source: Option[String],
-                                 tests: List[Map[String, String]]
+                                 devanaagarii_round_trip: List[Map[String, String]]
                                )
 
 class TransliterationTest extends FlatSpec {
-  val log = LoggerFactory.getLogger(this.getClass)
+  private val log: Logger = LoggerFactory.getLogger(this.getClass)
 
   "transliterator" should "work" in {
     val source = Source.fromResource("transliterationTests.json")
     implicit val formats = DefaultFormats
     val testJson = Serialization.read[TransliterationTests](source.mkString)
-    testJson.tests.foreach(test => {
-      log.debug(test.toString)
+    testJson.devanaagarii_round_trip.foreach(test => {
+      log.info(test.toString)
       test.filterKeys(_ != transliterator.scriptDevanAgarI).foreach {
         case (scheme: String, value: String) => {
-          log.info(scheme)
+          log.info(s"$scheme : $value")
           assert(transliterator.transliterate(in_str = test(transliterator.scriptDevanAgarI), sourceScheme = transliterator.scriptDevanAgarI, destScheme = scheme) == test(scheme))
           assert(transliterator.transliterate(in_str = test(scheme), sourceScheme = scheme, destScheme = transliterator.scriptDevanAgarI) == test(transliterator.scriptDevanAgarI))
         }
